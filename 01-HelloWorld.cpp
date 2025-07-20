ca58ee355182b5b56884d09ea869340c93d0a7a1
Samir Das <samir.das1055@gmail.com>
2025-07-20 21:12:57 +0530

    Two program

---
+++ 01-HelloWorld.cpp
@@ -0,0 +1,8 @@
+//01. Write a program to print "Hello, World!"
+#include<iostream>
+using namespace std;
+int main()
+{
+  cout << "Hello, World!" << endl;
+  return 0;
+}
---
+++ 02-SumTwoNumbers.cpp
@@ -0,0 +1,14 @@
+//2. Take user input for two numbers and print their sum.
+#include<iostream>
+using namespace std;
+int main()
+{
+  int num1, num2, sum;
+  cout << "Enter first number: ";
+  cin >> num1;
+  cout << "Enter second number: ";
+  cin >> num2;
+  sum = num1 + num2;
+  cout << "The sum of " << num1 << " and " << num2 << " is: " << sum << endl;
+  return 0;
+}
