31dfd281a76c4116b251680b6945cb9bf789bb58
Samir Das <samir.das1055@gmail.com>
2025-07-24 21:01:34 +0530

    3 & 4

---
+++ 04-FindLargestNumber.cpp
@@ -0,0 +1,26 @@
+//4. Find the largest of three numbers.
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a, b, c;
+  cout << "Enter three numbers: ";
+  cin >> a >> b >> c;
+
+  int largest;
+
+  if (a >= b && a >= c)
+  {
+    largest = a;
+  }
+  else if (b >= a && b >= c)
+  {
+    largest = b;
+  }
+  else
+  {
+    largest = c;
+  }
+  cout << "The largest number is: " << largest << endl;
+  return 0;
+}
